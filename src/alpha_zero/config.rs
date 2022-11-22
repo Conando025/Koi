e21d3f0cc8651f3ee7d9607eba23364c90d29360
conando <conando025@protonmail.com>
2022-11-22 17:52:15 +0100

    Config attributes are now public The game trait also underwent a minor revision

--- src/alpha_zero/config.rs
+++ src/alpha_zero/config.rs
@@ -1,16 +1,17 @@
+#[derive(Clone)]
 pub struct Config {
-    actor_count: usize,
-    move_samples: usize,
-    max_move_count: usize,
-    dirichlet_alpha: f64,           //0.3 for chess, 0.03 for Go and 0.15 for shogi.
-    exploration_fraction: f64,
-    c_base: usize,                  //for UCB formula
-    c_init: f64,                    //for UCB formula
-    training_steps: usize,
-    checkpoint_interval: usize,
-    window_size: usize,
-    batch_size: usize,
-    weight_decay: f64,
-    momentum: f64,
-    learning_rate_schedule: (usize, f64),
+    pub(super) actor_count: usize,
+    pub(super) move_samples: usize,
+    pub(super) max_move_count: usize,
+    pub(super) dirichlet_alpha: f64,           //0.3 for chess, 0.03 for Go and 0.15 for shogi.
+    pub(super) exploration_fraction: f64,
+    pub(super) c_base: usize,                  //for UCB formula
+    pub(super) c_init: f64,                    //for UCB formula
+    pub(super) training_steps: usize,
+    pub(super) checkpoint_interval: usize,
+    pub(super) window_size: usize,
+    pub(super) batch_size: usize,
+    pub(super) weight_decay: f64,
+    pub(super) momentum: f64,
+    pub(super) learning_rate_schedule: (usize, f64),
 }
--- src/alpha_zero/game.rs
+++ src/alpha_zero/game.rs
@@ -1,11 +1,16 @@
-trait Game: Clone {
-    type Action;
+pub trait Game: Clone {
+    type Action: Send + 'static;
     type Representation;
     fn create(history: Option<Vec<Self::Action>>) -> Self;
     fn terminal(&self) -> bool;
     fn terminal_value(&self) -> f64;
-    fn legal_actions(&self) -> Vec<Move>;
+    fn legal_actions(&self) -> Vec<Self::Action>;
     fn apply(&mut self, action: Self::Action);
     fn make_target(&self) -> Self::Representation;
-    fn to_play(&self) -> int;
+    fn to_play(&self) -> Player;
+}
+
+pub enum Player {
+    A,
+    B
 }
